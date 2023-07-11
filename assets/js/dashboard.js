52ca87a29549929dfff0931cdb050524eb182cce
Franck WANG <franckwang123@gmail.com>
2023-07-11 21:49:28 +0200

    assets

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -1,4 +1,4 @@
-/*$(function () {
+$(function () {
 
 
   // =====================================
@@ -208,4 +208,4 @@
     },
   };
   new ApexCharts(document.querySelector("#earning"), earning).render();
-})*/
+})
